9992fddc9cf46765a12dd9513bcc92294624afbd
Tksi <24800668+Tksi@users.noreply.github.com>
2025-11-25 18:31:07 +0900

    feat(frontend): init

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,24 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxt/eslint'],
+  compatibilityDate: '2025-10-19',
+  devtools: { enabled: false },
+  ssr: false,
+  experimental: {
+    typedPages: true,
+  },
+  typescript: {
+    tsConfig: {
+      compilerOptions: {
+        noUncheckedIndexedAccess: true,
+        exactOptionalPropertyTypes: true,
+        noImplicitAny: true,
+        noImplicitReturns: true,
+        noImplicitOverride: true,
+        noUnusedLocals: true,
+        noUnusedParameters: true,
+        noFallthroughCasesInSwitch: true,
+      },
+    },
+  },
+});
